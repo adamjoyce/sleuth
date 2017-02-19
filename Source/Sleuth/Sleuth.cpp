bdda168b5ba71e665f335c524a37a354bb31b04f
Adam Joyce <adamjoyce@outlook.com>
2017-02-19 12:16:14 +0000

    UE4 project setup

---
+++ Source/Sleuth/Sleuth.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Sleuth.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sleuth, "Sleuth" );
---
+++ Source/Sleuth/SleuthGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SleuthGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLEUTH_API ASleuthGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
