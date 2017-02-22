4daddf591181710bc2fae3f3c3fece8748314b1e
Adam Joyce <adamjoyce@outlook.com>
2017-02-22 20:03:19 +0000

    Generally failed with my circular health bar blueprint implementation - think I will switch to code

--- Source/Sleuth/BaseCharacter.h
+++ Source/Sleuth/BaseCharacter.h
@@ -15,18 +15,18 @@ public:
 	ABaseCharacter();
 
 	/* Returns the player's current health. */
-	UFUNCTION()
+	UFUNCTION(BlueprintCallable, Category="PlayerCondition")
 	float GetHealth() const;
 
 	/* Is the character above zero health? */
 	UFUNCTION()
 	bool IsAlive() const;
 
-protected:
-	/* The character's health pool - zero means game over. */
-	UPROPERTY(EditAnywhere, Category = "PlayerCondition")
-	float Health;
+    /* The character's health pool - zero means game over. */
+    UPROPERTY(EditAnywhere, Category = "PlayerCondition")
+    float Health;
 
+protected:
 	/* Damage. */
 	virtual float TakeDamage(float Damage, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, class AActor* DamageCauser) override;
 };
